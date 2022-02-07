6a732a9fc5116cea4cbbc89d044b7084cce051b1
Andre <andre@A-MacBook-Air.local>
2022-02-07 00:08:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  state = {
+    luke: "",
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://swapi.dev/api/people/1")
+      .then((res) => {
+        this.setState({ luke: res.data });
+        console.log(res.data);
+      })
+      .catch((e) => console.log("error", e));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Name: {this.state.luke.name}</h1>
+        <h1>Birth Year: {this.state.luke.birth_year}</h1>
+        <h1>Height: {this.state.luke.height}</h1>
+        <h1>Eye Color: {this.state.luke.eye_color}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
